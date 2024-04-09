acabbd5e3c96b5d743c4bd636abc8b6ad8bd1239
bestiadivina147 <diegopc147@gmail.com>
2024-04-09 13:10:34 +0200

    feat:

---
+++ src/main/java/edu/badpals/arnoldenumtype/main/ArnoldMain.java
@@ -0,0 +1,13 @@
+package edu.badpals.arnoldenumtype.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArnoldMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edu/badpals/arnoldenumtype/AppTest.java
@@ -0,0 +1,20 @@
+package edu.badpals.arnoldenumtype;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
